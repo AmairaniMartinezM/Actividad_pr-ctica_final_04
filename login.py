6d94a63b0fad84bc2406d99b0b94cfedc1cdda62
Amairani Martínez M <1722110355@utectulancingo.edu.mx>
2025-10-16 22:36:27 -0600

    Add login.py placeholder

---
+++ login.py
@@ -0,0 +1,9 @@
+﻿# login module
+
+def login(username, password):
+    """Placeholder login function.
+    Returns True if username and password are non-empty strings.
+    """
+    if not username or not password:
+        return False
+    return True
